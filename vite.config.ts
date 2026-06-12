9c298486f419b8d719bc48a731a57dbb92e25157
WhXteBeXr <99601495+WhXteBeXr@users.noreply.github.com>
2026-06-12 19:41:22 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  base: '/repo-name/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
