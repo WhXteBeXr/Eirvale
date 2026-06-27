6e711fee3d5a93684c8357d54d7221dda2aa6dbd
WhXteBeXr <WhiteBear_LT@outlook.com>
2026-06-28 00:36:26 +0300

    suitable "base" been edited

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/repo-name/',
+  base: '/Eirvale/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
