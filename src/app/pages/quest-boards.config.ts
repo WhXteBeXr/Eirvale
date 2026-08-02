b4e360f042904ed5d1fe49847ad9eea525672c1d
WhXteBeXr <WhiteBear_LT@outlook.com>
2026-08-03 00:42:00 +0300

    Working on board creation method in quest boards class

--- src/app/pages/quest-boards.config.ts
+++ src/app/pages/quest-boards.config.ts
@@ -1,40 +1,48 @@
+type KindOfDate = 'creation' | 'expiration';
+
+export type QBPageElement = QuestCard | QuestColumn | QuestBoard;
+
+export interface Date {
+  day: string;
+  month: string;
+  year: string;
+}
+
+interface QuestDate {
+  kind: KindOfDate;
+  date?: Date;
+}
+
+interface QBBaseElement {
+  id: number;
+  title?: string;
+  description?: string;
+}
+
+interface QBElement<TChild extends QBPageElement> extends QBBaseElement {
+  children: TChild[];
+}
+
+export interface QuestCard extends QBElement<never> {
+  rewards: string[];
+  dates: {
+    creation: QuestDate;
+    expiration: QuestDate;
+  };
+}
+
+export interface QuestColumn extends QBElement<QuestCard> {
+  importance: string;
+  totalQuests?: number;
+}
+
+export interface QuestBoard extends QBElement<QuestColumn> {
+  totalColumns?: number;
+}
+
 export interface ToolbarAction {
   id: number;
   name: string;
   title?: string;
   description?: string;
 }
-
-export interface QuestDate {
-  day: string;
-  month: string;
-  year: string;
-}
-
-export interface QuestCard {
-  id: number;
-  className: string;
-  title: string;
-  description: string;
-  rewards: string[];
-  creationDate?: QuestDate;
-  expirationDate?: QuestDate;
-}
-
-export interface QuestColumn {
-  id: number;
-  className: string;
-  title: string;
-  importance: string;
-  listedQuests: QuestCard[];
-  totalQuests?: number;
-}
-
-export interface QuestBoard {
-  id: number;
-  className: string;
-  title: string;
-  questColumns: QuestColumn[];
-}
-
-export type QBElementInfo = QuestBoard | QuestColumn | QuestCard;
--- src/app/pages/quest-boards.ts
+++ src/app/pages/quest-boards.ts
@@ -6,6 +6,8 @@ import type {
   QuestBoard,
   QBElementInfo,
   ToolbarAction,
+  KindOfDate,
+  QBPageElement,
 } from '@/app/pages/quest-boards.config.ts';
 
 export class QuestBoardPage extends Page {
@@ -17,21 +19,64 @@ export class QuestBoardPage extends Page {
     super(mountContainer, toolbarContainer, toolbarActions);
   }
 
+  // Создание всего лэйаута страницы
   protected createPageLayout(): HTMLElement {
     return undefined;
   }
 
+  // Обработчик действий по клику кнопки внутри тулбара
   protected handleToolbarAction(button: HTMLButtonElement) {}
 
+  // TODO: Пересмотреть метод. Может стоит сделать рекурсию, которая разворачивалась бы от списка элементов до одного простого
+  // Универсальный метод создания элемента
   private createElement<K extends keyof HTMLElementTagNameMap>(
     elementName: K,
-    elementInfo: QBElementInfo,
+    elementData: QBPageElement,
   ): HTMLElement {
     const element: HTMLElement = document.createElement(elementName);
-    element.classList.add(`quest-board__${elementInfo.className}`);
+
+    return element;
   }
 
-  private insertData(): void {}
+  private createQuestCard(questCardData: QuestCard): HTMLElement {
+    return this.createElement('li', questCardData);
+  }
 
-  private formatDate({ day, month, year }: QuestDate): string {}
+  private createColumn(columnData: QuestColumn): HTMLElement {
+    const column = this.createElement('ul', columnData);
+    columnData.children.forEach((child) => {
+      column.appendChild(this.createQuestCard(child));
+    });
+    return column;
+  }
+
+  // Основной метод, создающий доску квестов
+  private createBoard(elementData: QuestBoard): HTMLElement {
+    const board = this.createElement('section', elementData);
+    elementData.children.forEach((child) => {
+      board.appendChild(this.createColumn(child));
+    });
+    return board;
+  } // TODO: Пересмотреть создание доски. Возможно переделать на рекурсию
+
+  // Вложение данных в переданный контейнер
+  // private fillElement(element: HTMLElement, elementInfo: QBElementInfo): void {
+  //   element.classList.add(`quest-board__${elementInfo.className}`);
+  // }
+
+  // Форматирование даты
+  private formatDate(kind: KindOfDate, date: QuestDate | undefined): string {
+    if (date) {
+      const { day, month, year } = date;
+      return kind === 'creation'
+        ? `Was created on: ${day}/${month}/${year}`
+        : `Expire on:: ${day}/${month}/${year}`;
+    } else {
+      return kind === 'creation'
+        ? `Doesn't have creation date`
+        : `Doesn't have expiration date`;
+    }
+  }
+
+  // TODO: Реализовать прослойки для создания элементов страницы (зависит от выбора подхода создания)
 }
