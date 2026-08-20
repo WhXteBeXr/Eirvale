146c1b09c874aa33951ff4d6540f4c75da4bbaae
WhXteBeXr <WhiteBear_LT@outlook.com>
2026-08-20 21:05:49 +0300

    Config file fixes

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,31 +1,42 @@
+import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tsEslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['dist', 'node_modules'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
   },
 
   js.configs.recommended,
-
-  ...tseslint.configs.recommended,
+  tsEslint.configs.recommended,
 
   {
     files: ['**/*.ts'],
+
     rules: {
       'no-console': 'warn',
       'no-debugger': 'warn',
+      'no-unused-vars': 'off',
 
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
         },
       ],
+
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/explicit-function-return-type': 'off',
+
       '@typescript-eslint/consistent-type-imports': [
         'error',
         { prefer: 'type-imports' },
